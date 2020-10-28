96efece1226175a426759b33f90ca741b289038e
soumavm <soumavm@gmail.com>
2020-10-28 11:08:51 -0600

    Made project

---
+++ Connect4/cmake-build-debug/CMakeFiles/Connect4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Connect4.dir/main.cpp.obj"
+  "Connect4.exe"
+  "Connect4.exe.manifest"
+  "Connect4.pdb"
+  "libConnect4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Connect4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
