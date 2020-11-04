26f235375047bbb8b5a5227488540c885b35ff79
soumavm <soumavm@gmail.com>
2020-11-03 22:39:21 -0700

    Converted all code in Main into seperate functions

--- Connect4/main.cpp
+++ Connect4/main.cpp
@@ -6,14 +6,43 @@
 #include <array>
 
 using namespace std;
-string Board[6][7];
+
+
 bool Win = false;
 int Placement=1, Turn = 1, count=0, countRight=0, countLeft=0;
 
+//Initializing functions
+void fillBoard(string Board[6][7]);
+void displayBoard(string Board[6][7]);
+void placeInput(string Board[6][7]);
+void checkHorizontal(string Board[6][7]);
+void checkVertical(string Board[6][7]);
+void checkDiagonal(string Board[6][7]);
 
 int main()
 {
-    //fills game Board with blanks
+    string Board[6][7];
+    //fills game Board with blanks & Displays it
+    fillBoard(Board);
+    displayBoard(Board);
+
+    while(!Win)
+    {
+        //Places down user inputs & displays Board
+        placeInput(Board);
+        displayBoard(Board);
+        //checks for horizontal win
+        checkHorizontal(Board);
+        //checks for vertical win
+        checkVertical(Board);
+        //Diagonal Win
+        checkDiagonal(Board);
+    }
+
+}
+
+void fillBoard(string Board[6][7])
+{
     for(int Row=0; Row<6; Row++)
     {
         for(int Column=0; Column<7; Column++)
@@ -21,7 +50,10 @@ int main()
             Board[Row][Column] = " - ";
         }
     }
+}
 
+void displayBoard(string Board[6][7])
+{
     for(int Row=0; Row<6; Row++)
     {
         for(int Column=0; Column<7; Column++)
@@ -30,126 +62,112 @@ int main()
         }
         cout<<endl;
     }
+}
 
+void placeInput(string Board[6][7])
+{
+    cout << "Player " << Turn << "'s Turn"<<endl << "Enter Column Placement:";
+    cin>>Placement;
+    Turn = Turn%2;
 
-    while(!Win)
+    for(int Row_Placed=6; Row_Placed>0; Row_Placed--)
     {
-        //Places down user inputs
-        cout << "Player " << Turn << "'s Turn"<<endl << "Enter Column Placement:";
-        cin>>Placement;
-        Turn = Turn%2;
-
-        for(int Row_Placed=6; Row_Placed>0; Row_Placed--)
+        if(Board[Row_Placed-1][Placement-1]==" - ")
         {
-            if(Board[Row_Placed-1][Placement-1]==" - ")
+            if(Turn == 1)
             {
-                if(Turn == 1)
-                {
-                    Board[Row_Placed-1][Placement-1] = " X ";
-                    break;
-                }
+                Board[Row_Placed-1][Placement-1] = " x ";
+                break;
+            }
+            else
+            {
+                Board[Row_Placed-1][Placement-1] = " o ";
+                break;
+            }
+        }
+    }
+    Turn++;
+}
+
+void checkHorizontal(string Board[6][7])
+{
+    for(int Row_Check =5; Row_Check>-1; Row_Check--)
+    {
+        for(int Column=1; Column<7;Column++)
+        {
+            if((Board[Row_Check][Column-1]==Board[Row_Check][Column]) &&
+               (Board[Row_Check][Column]!=" - "))
+                count++;
+            else
+                count=0;
+            if(count==3)
+            {
+                if(Board[Row_Check][Column]==" O ")
+                    cout << "Player O has won with Horizontal.";
                 else
-                {
-                    Board[Row_Placed-1][Placement-1] = " O ";
-                    break;
-                }
+                    cout << "Player X has won with Horizontal.";
+                Win = true;
+                break;
             }
 
         }
-
-        Turn++;
-
-        //Displays Board
-        for(int Row=0; Row<6; Row++)
+        count=0;
+    }
+}
+void checkVertical(string Board[6][7])
+{
+    for(int Col_Check =6; Col_Check>-1; Col_Check--)
+    {
+        for(int Row=1; Row<6;Row++)
         {
-            for(int Column=0; Column<7; Column++)
+            if((Board[Row-1][Col_Check]==Board[Row][Col_Check]) && (Board[Row][Col_Check]!=" - "))
+                count++;
+
+            else
+                count=0;
+
+            if(count==3)
             {
-                cout << Board[Row][Column];
+                if(Board[Row][Col_Check]==" O ") cout << "Player O has won with vertical.";
+                else cout << "Player X has won with Vertical.";
+                Win = true;
+                break;
             }
-            cout<<endl;
+
         }
-
-        //checks for horizontal win
-        for(int Row_Check =5; Row_Check>-1; Row_Check--)
-        {
-            for(int Column=1; Column<7;Column++)
-            {
-                if((Board[Row_Check][Column-1]==Board[Row_Check][Column]) &&
-                   (Board[Row_Check][Column]!=" - "))
-                {
-                    count++;
-                }
-
-                else
-                    count=0;
-
-                if(count==3)
-                {
-                    if(Board[Row_Check][Column]==" O ") cout << "Player O has won with Horizontal.";
-                    else cout << "Player X has won with Horizontal.";
-                    Win = true;
-                    break;
-                }
-
-            }
-            count=0;
-        }
-
-        //checks for vertical win
-        for(int Col_Check =6; Col_Check>-1; Col_Check--)
-        {
-            for(int Row=1; Row<6;Row++)
-            {
-                if((Board[Row-1][Col_Check]==Board[Row][Col_Check]) && (Board[Row][Col_Check]!=" - "))
-                    count++;
-
-                else
-                    count=0;
-
-                if(count==3)
-                {
-                    if(Board[Row][Col_Check]==" O ") cout << "Player O has won with vertical.";
-                    else cout << "Player X has won with Vertical.";
-                    Win = true;
-                    break;
-                }
-
-            }
-            count=0;
-        }
-
-        //Diagonal Win
-
-        for(int Row_Dia_Check =5; Row_Dia_Check>2; Row_Dia_Check--)
-        {
-            for(int Col_Dia_Check=0; Col_Dia_Check<7;Col_Dia_Check++)
-            {
-                for(int Diagonal=1; Diagonal<4; Diagonal++)
-                {
-                    if(Board[Row_Dia_Check][Col_Dia_Check]==Board[Row_Dia_Check-Diagonal][Col_Dia_Check+Diagonal] && (Board[Row_Dia_Check][Col_Dia_Check]!=" - "))
-                        countRight++;
-                    else
-                        countRight=0;
-
-                    if(Board[Row_Dia_Check][Col_Dia_Check]==Board[Row_Dia_Check-Diagonal][Col_Dia_Check-Diagonal] && (Board[Row_Dia_Check][Col_Dia_Check]!=" - "))
-                        countLeft++;
-                    else
-                        countLeft=0;
-
-                    if(countRight==3 || countLeft==3)
-                    {
-                        if(Board[Row_Dia_Check][Col_Dia_Check]==" O ") cout << "Player O has won with Diagonal";
-                        else cout << "Player X has won with Diagonal";
-                        Win = true;
-                        break;
-                    }
-
-                }
-                countLeft=0;
-                countRight=0;
-            }
-        }
-
+        count=0;
     }
 
+}
+void checkDiagonal(string Board[6][7])
+{
+    for(int Row_Dia_Check =5; Row_Dia_Check>2; Row_Dia_Check--)
+    {
+        for(int Col_Dia_Check=0; Col_Dia_Check<7;Col_Dia_Check++)
+        {
+            for(int Diagonal=1; Diagonal<4; Diagonal++)
+            {
+                if(Board[Row_Dia_Check][Col_Dia_Check]==Board[Row_Dia_Check-Diagonal][Col_Dia_Check+Diagonal] && (Board[Row_Dia_Check][Col_Dia_Check]!=" - "))
+                    countRight++;
+                else
+                    countRight=0;
+
+                if(Board[Row_Dia_Check][Col_Dia_Check]==Board[Row_Dia_Check-Diagonal][Col_Dia_Check-Diagonal] && (Board[Row_Dia_Check][Col_Dia_Check]!=" - "))
+                    countLeft++;
+                else
+                    countLeft=0;
+
+                if(countRight==3 || countLeft==3)
+                {
+                    if(Board[Row_Dia_Check][Col_Dia_Check]==" O ") cout << "Player O has won with Diagonal";
+                    else cout << "Player X has won with Diagonal";
+                    Win = true;
+                    break;
+                }
+
+            }
+            countLeft=0;
+            countRight=0;
+        }
+    }
 }
